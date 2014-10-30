3dc9edc59fd7384950a72631419dc54d54eb9725
purplepapa <wenbo707@vip.sina.com>
2014-10-30 15:06:53 +0800

    project init

---
+++ src/main/java/com/github/purplepapa/Storm_Simple_Crawler/App.java
@@ -0,0 +1,13 @@
+package com.github.purplepapa.Storm_Simple_Crawler;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

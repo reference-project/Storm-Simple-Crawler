299fbce6f6acbecf90dbeea57ba55b75aab1b338
purplepapa <wenbo707@vip.sina.com>
2014-10-30 15:19:29 +0800

    init pom.xml

---
+++ src/main/java/com/github/purplepapa/Storm_Simple_Crawler/CrawlerTopology.java
@@ -0,0 +1,10 @@
+package com.github.purplepapa.Storm_Simple_Crawler;
+
+public class CrawlerTopology {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 System.out.println( "Hello World!" );
+	}
+
+}

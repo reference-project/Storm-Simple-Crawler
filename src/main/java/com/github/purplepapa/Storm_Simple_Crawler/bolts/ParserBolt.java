b1434dfaf5563f13b015ebc93814f282ea36cfaa
purplepapa <wenbo707@vip.sina.com>
2014-10-31 13:32:45 +0800

    test

---
+++ src/main/java/com/github/purplepapa/Storm_Simple_Crawler/bolts/ParserBolt.java
@@ -0,0 +1,29 @@
+package com.github.purplepapa.Storm_Simple_Crawler.bolts;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import backtype.storm.task.OutputCollector;
+import backtype.storm.task.TopologyContext;
+import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.topology.base.BaseRichBolt;
+import backtype.storm.tuple.Fields;
+import backtype.storm.tuple.Tuple;
+import backtype.storm.tuple.Values;
+
+public class ParserBolt extends BaseRichBolt {
+	private OutputCollector collector;
+
+	public void prepare(Map config, TopologyContext context,
+			OutputCollector collector) {
+		this.collector = collector;
+	}
+
+	public void execute(Tuple tuple) {
+	}
+
+	public void declareOutputFields(OutputFieldsDeclarer declarer) {
+		
+	}
+
+}
---
+++ src/main/java/com/github/purplepapa/Storm_Simple_Crawler/bolts/URLDeduplicatorBolt.java
@@ -0,0 +1,30 @@
+package com.github.purplepapa.Storm_Simple_Crawler.bolts;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import backtype.storm.task.OutputCollector;
+import backtype.storm.task.TopologyContext;
+import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.topology.base.BaseRichBolt;
+import backtype.storm.tuple.Fields;
+import backtype.storm.tuple.Tuple;
+import backtype.storm.tuple.Values;
+
+public class URLDeduplicatorBolt extends BaseRichBolt {
+	private OutputCollector collector;
+	
+	public void prepare(Map config, TopologyContext context,
+			OutputCollector collector) {
+		this.collector = collector;
+	}
+
+	public void execute(Tuple tuple) {
+		System.out.println("in url dedup:"+tuple.toString());
+	}
+
+	public void declareOutputFields(OutputFieldsDeclarer declarer) {
+//		declarer.declare(new Fields("word", "count"));
+	}
+
+}
